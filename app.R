71ca9fbb067706c6a10373d546b26b0c0ee15389
Ziji Zhou <zijzhou25@amherst.edu>
2021-10-20 01:42:15 -0400

    Final Commit

--- app.R
+++ app.R
@@ -7,6 +7,9 @@
 #throughout the code, you will see some comments saying our names to signify where each of us worked primarily
 #however, we did do the majority of this project together.
 
+#For the third tab, the website we scraped from changed their data on Wednesday due to the new NBA season starting. 
+#We adjusted our data to reflect career stats but added a tab with the initial data that we scraped before the change. This data is not reproducible anymore however the career tab is.
+
 #load libraries
 library(shiny)
 library(tidyverse)
@@ -24,8 +27,16 @@ nba_salaries <- read.csv("data/nba_salaries.csv")
 teams <- unique(nba_salaries$team)
 #for radio buttons
 stat_choice_values <- c("games", "points", "rebounds", "assists")
-stat_choice_names <- c("Games", "Points Per Game", "Rebounds Per Game", "Assists Per Game")
+stat_choice_names <- c("Total Games", "Career Points Per Game", "Career Rebounds Per Game", "Career Assists Per Game")
 names(stat_choice_values) <- stat_choice_names
+
+#Backup data before it changed on Wednesday
+nba_salaries2 <- read.csv("data-backup/nba_salaries.csv")
+teams2 <- unique(nba_salaries$team)
+#for radio buttons
+stat_choice_values2 <- c("games", "points", "rebounds", "assists")
+stat_choice_names2 <- c("Games", "Points Per Game", "Rebounds Per Game", "Assists Per Game")
+names(stat_choice_values2) <- stat_choice_names2
 # Define UI 
 ui <- navbarPage(
     title = "Basketball Economics",
@@ -53,7 +64,7 @@ ui <- navbarPage(
     ),
     
     tabPanel(
-        title = "Do Higher Paid Players Produce?",
+        title = "Do Higher Paid Players Produce? (Career)",
         #getting a selector for teams to select
         sidebarLayout(
             sidebarPanel(
@@ -71,7 +82,28 @@ ui <- navbarPage(
                 mainPanel(plotOutput(outputId = "NBA_salaries_plot"))
                 
             )
+        ),
+    
+    tabPanel(
+        title = "Do Higher Paid Players Produce? (2020-2021)",
+        #getting a selector for teams to select
+        sidebarLayout(
+            sidebarPanel(
+                selectizeInput(
+                    inputId = "id2",
+                    label = "What teams do you want to show?",
+                    choices = teams2,
+                    selected = NULL,
+                    multiple = TRUE),
+                # getting selector for which stat we want shown
+                radioButtons(inputId = "pt_size2",
+                             label = "Which Stat do you Want?",
+                             choices = stat_choice_values2,
+                             selected = NULL)),
+            mainPanel(plotOutput(outputId = "NBA_salaries_plot2"))
+            
         )
+    )
 )
     
     #ROHIL (dont forget to add commas)
@@ -120,6 +152,25 @@ server <- function(input, output) {
                 y = stat_choice_names[stat_choice_values == input$pt_size]
             )
     })
+    #Ziji 
+    salary_data2 <- reactive({
+        data <- nba_salaries2 %>%
+            filter(team %in% input$id2)
+    })
+    output$NBA_salaries_plot2 <- renderPlot({
+        salary_data2() %>%
+            #plotting the data using a reactive element for the y axis
+            ggplot(aes_string(x = "salaryProportion", y = input$pt_size2)) +
+            #having different colors and changing size of the points
+            geom_point(aes(color = team), size = 6) +
+            #adding labels
+            labs(
+                title = "Players' stats vs. Salary",
+                x = "Ratio of Player's Salary to Median Income in Team's City",
+                y = stat_choice_names2[stat_choice_values2 == input$pt_size2]
+            )
+    })
+    
 }
 
 # Run the application 
